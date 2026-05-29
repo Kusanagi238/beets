c8cea3b5ce207f8d5c350753328b3ce99632fbe9
Sebastian Mohr <sebastian@mohrenclan.de>
2026-05-29 15:01:42 +0200

    filefilter: ImportTestCase -> PytestImportHelper and fixture based setup

--- test/plugins/test_filefilter.py
+++ test/plugins/test_filefilter.py
@@ -14,16 +14,18 @@
 
 """Tests for the `filefilter` plugin."""
 
-from beets.test.helper import ImportTestCase, PluginMixin
+import pytest
+
+from beets.test.helper import PluginMixin, PytestImportHelper
 from beets.util import bytestring_path
 
 
-class FileFilterPluginMixin(PluginMixin, ImportTestCase):
+class PytestFileFilterHelper(PluginMixin, PytestImportHelper):
     plugin = "filefilter"
     preload_plugin = False
 
-    def setUp(self):
-        super().setUp()
+    @pytest.fixture(autouse=True)
+    def filefilter_setup(self, setup_import):
         self.prepare_tracks_for_import()
 
     def prepare_tracks_for_import(self):
@@ -49,9 +51,9 @@ class FileFilterPluginMixin(PluginMixin, ImportTestCase):
         assert {i.path for i in self.lib.items()} == expected_paths
 
 
-class FileFilterPluginNonSingletonTest(FileFilterPluginMixin):
-    def setUp(self):
-        super().setUp()
+class TestFileFilterPluginNonSingleton(PytestFileFilterHelper):
+    @pytest.fixture(autouse=True)
+    def non_singleton_setup(self, filefilter_setup):
         self.importer = self.setup_importer(autotag=False, copy=False)
 
     def test_import_default(self):
@@ -76,9 +78,9 @@ class FileFilterPluginNonSingletonTest(FileFilterPluginMixin):
         self._run({"singleton_path": ".*other_album.*"}, 3, self.all_tracks)
 
 
-class FileFilterPluginSingletonTest(FileFilterPluginMixin):
-    def setUp(self):
-        super().setUp()
+class TestFileFilterPluginSingleton(PytestFileFilterHelper):
+    @pytest.fixture(autouse=True)
+    def singleton_setup(self, filefilter_setup):
         self.importer = self.setup_singleton_importer(autotag=False, copy=False)
 
     def test_global_config(self):
