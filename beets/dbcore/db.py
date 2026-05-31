0b9bc6e3604b0ad32bd47345805e0ae08f1704ed
Šarūnas Nejus <snejus@protonmail.com>
2026-05-31 09:18:12 +0100

    Return KeysView from every keys method

--- beets/dbcore/db.py
+++ beets/dbcore/db.py
@@ -60,8 +60,10 @@ if TYPE_CHECKING:
         Generator,
         Iterable,
         Iterator,
+        KeysView,
         Sequence,
     )
+    from collections.abc import Set as AbstractSet
     from sqlite3 import Connection
     from types import TracebackType
 
@@ -112,22 +114,23 @@ class FormattedMapping(Mapping[str, str]):
     """
 
     model: Model
+    model_keys: AbstractSet[str]
 
     ALL_KEYS = "*"
 
     def __init__(
         self,
         model: Model,
-        included_keys: str = ALL_KEYS,
+        included_keys: str | list[str] = ALL_KEYS,
         for_path: bool = False,
-    ):
+    ) -> None:
         self.for_path = for_path
         self.model = model
         if included_keys == self.ALL_KEYS:
             # Performance note: this triggers a database query.
             self.model_keys = self.model.keys(True)
         else:
-            self.model_keys = included_keys
+            self.model_keys = set(included_keys)
 
     def __getitem__(self, key: str) -> str:
         if key in self.model_keys:
@@ -575,23 +578,23 @@ class Model(ABC, Generic[D]):
         else:
             raise KeyError(f"no such field {key}")
 
-    def keys(self, computed: bool = False):
+    def keys(self, computed: bool = False) -> KeysView[str]:
         """Get a list of available field names for this object. The
         `computed` parameter controls whether computed (plugin-provided)
         fields are included in the key list.
         """
-        base_keys = list(self._fields) + list(self._values_flex.keys())
+        keys = {*self._fields, *self._values_flex}
         if computed:
-            return base_keys + list(self._getters().keys())
-        else:
-            return base_keys
+            keys.update(self._getters())
+
+        return dict.fromkeys(keys).keys()
 
     @classmethod
-    def all_keys(cls):
+    def all_keys(cls) -> KeysView[str]:
         """Get a list of available keys for objects of this type.
         Includes fixed and computed fields.
         """
-        return list(cls._fields) + list(cls._getters().keys())
+        return dict.fromkeys({*cls._fields, *cls._getters()}).keys()
 
     # Act like a dictionary.
 
